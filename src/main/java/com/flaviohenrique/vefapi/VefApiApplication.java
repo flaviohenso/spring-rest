e9251bd3d5b4258434f0c6f4ed03f5a4f79fa9d7
Flavio Henrique S Cabral <flavio.henso@gmail.com>
2020-04-24 12:33:21 -0300

    Criando projeto Spring rest - Estudo

---
+++ src/main/java/com/flaviohenrique/vefapi/VefApiApplication.java
@@ -0,0 +1,13 @@
+package com.flaviohenrique.vefapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VefApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VefApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flaviohenrique/vefapi/VefApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviohenrique.vefapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VefApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
